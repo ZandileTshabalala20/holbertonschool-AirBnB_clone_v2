75eec5ed54d2879185f55c6832ed8efb79174c70
ZandileTshabalala20 <zandiletsh20@gmail.com>
2022-08-09 16:48:01 +0200

    commit

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,21 @@
-#!/usr/bin/python3
-""" Anenity class definition """
-from models.base_model import Base
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """Amenity model"""
-    __tablename__ = "amenities"
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-#        place_amenities = relationship("Place", secondary="place_amenity",y                                      viewonly=False)
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
