2cbbc493ee733282c9b873e2dbb9e6381bed6fca
ZandileTshabalala20 <zandiletsh20@gmail.com>
2022-08-09 16:26:31 +0200

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from os import getenv
+'''
+    Package initializer
+'''
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import os
 
-storage_fdb = getenv("HBNB_TYPE_STORAGE")
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
 
-if storage_fdb == 'db':
-    from models.engine.db_storage import DBStorage
+if os.getenv('HBNB_TYPE_STORAGE') == "db":
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage
+    storage = FileStorage()
 storage.reload()
--- models/city.py
+++ models/city.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+'''
+    Define the class City.
+'''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, DateTime, ForeignKey
+import models
+from os import environ
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    __tablename__ = "cities"
+    if environ.get("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        name = ""
+        state_id = ""
